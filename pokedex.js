e35b3406d1d2d27dbeed9b26f124c906a8e1ba56
Joonas Iivonen <joonas.iivonen@gmail.com>
2025-02-28 09:51:31 +0200

    Basic code structure

--- pokedex.js
+++ pokedex.js
@@ -1,2 +1,26 @@
 import express from 'express';
 import fetch from 'node-fetch';
+
+const port = 3000;
+const host = 'localhost';
+
+const app = express();
+
+app.use('/inc', express.static('includes'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+app.get('/sukupolvi/:numero', (req, res) => {
+
+  res.render('generation');
+});
+
+app.get('/pokemon/:nimi', (req, res) => {
+
+  res.render('pokemon');
+});
+
+app.listen(port, host, () => console.log(`${host}:${port} kuuntelee...`));
